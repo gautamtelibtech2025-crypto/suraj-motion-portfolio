0a27d88a582e6f522624aae4456a95e261119864
Gautam <gautamteli.btech2025@spsu.ac.in>
2026-08-08 10:03:25 +0530

    Support Vercel root deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/suraj-motion-portfolio/",
+  base: process.env.VERCEL ? "/" : "/suraj-motion-portfolio/",
   plugins: [react()],
 });
